b0542f6fde5587b7cb3c0eee1cb9b1692ff9b7b1
jmebia <jmebia@wyvern.com.ph>
2020-04-14 14:13:15 +0800

    improved email validation and demo

--- js/validator.js
+++ js/validator.js
@@ -6,18 +6,32 @@ class Validator {
     constructor() {
         this.initRequired();
         this.initCurrencies();
+        this.initEmails();
     }
 
-    // binds a function to the form field that checks whether the field's value is mpty or not whenever focus on the field is lost
+    // setters
+    set customEmail(email) {
+        if (email.charAt(0) == "@") this.email = email;
+    }
+
+    // getters
+    get customEmail() {
+        return this.email;
+    }
+
+    // binds a function to the form field that checks whether the field's value is empty or not whenever focus on the field is lost
     initRequired() {
         $(".required").each(function (index) {
-            console.log("Adding empty validator for " + $(this));
+            // DEBUG console.log("Adding empty validator for " + $(this));
 
             $(this).focusout(function () {
-                console.log("input", $(this));
+                // DEBUG console.log("input", $(this));
                 $(this).removeClass('is-invalid');
-                if ($(this).val() == "")
+                $("#error-required-"+index).remove();
+                if ($(this).val() == "") {
                     $(this).addClass('is-invalid');
+                    $(this).after("<li class='small text-danger' id='error-required-"+index+"'>This field is required!</li>");
+                }
             });
         });
     }
@@ -28,40 +42,71 @@ class Validator {
             var regex = /^\d+(?:\.\d{0,2})$/;
 
             $(this).focusout(function () {
-                console.log("Adding empty validator for ", $(this));
-                console.log("Input String", $(this).val());
+                // DEBUG console.log("Adding empty validator for ", $(this));
+                // DEBUG console.log("Input String", $(this).val());
                 $(this).removeClass('is-invalid');
+                $("#error-currency-"+index).remove();
                 if (!regex.test($(this).val())) {
                     $(this).val((parseFloat($(this).val()).toFixed(2)).toString());
                     console.log("Fixed String", $(this).val());
+                    if (!regex.test($(this).val())) {
+                        $(this).addClass("is-invalid");
+                        $(this).after("<li class='small text-danger' id='error-currency-"+index+"'>Please input a valid currency value!</li>");
+                    }
                 }
             });
         });
-
     }
 
-    set customEmail(email) {
-        if (email.charAt(0) == "@") this.email = email;
-    }
+    initCustomEmails() {
+        var email = this.customEmail;
+        $(".validate-custom-email").each(function (index) {
+            $(this).focusout(function () {
+                // DEBUG console.log("Adding empty validator for ", $(this));
+                // DEBUG console.log("Checking email fields..");
+                $("#error-custom-email-"+index).remove();
+                $(this).removeClass("is-invalid");
 
-    get customEmail() {
-        return this.email;
-    }
-
-    // clear all validation messages
-    clearMessages() {
-        $(".validator-message").each(function (index) {
-            $(this).html("");
+                console.log($(this).val() + "  ??  " + email);
+                if ($(this).val().indexOf(email) !== -1) {
+                    //
+                } else {
+                    $(this).after("<li class='small text-danger' id='error-custom-email-"+index+"'>Email should be <b>___"+email+"</b></li>")
+                    $(this).addClass("is-invalid");
+                }
+            });
         });
     }
 
-    // call this function before sending your post request
+    initEmails() {
+        var email = this.customEmail;
+        $(".validate-email").each(function (index) {
+            var emailPattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+            $(this).focusout(function () {
+                // DEBUG console.log("Adding empty validator for ", $(this));
+                // DEBUG console.log("Checking email fields..");
+                $("#error-email-"+index).remove();
+                $(this).removeClass("is-invalid");
+
+                console.log($(this).val() + "  ??  " + email);
+                if (emailPattern.test($(this).val())) {
+                    //
+                } else {
+                    $(this).after("<li class='small text-danger' id='error-email-"+index+"'>Please use a proper Email format!</li>")
+                    $(this).addClass("is-invalid");
+                }
+            });
+        });
+    }
+
+    // call this function before sending your post requests
     checkValidations() {
         var isValid = true;
         var isEmailValid = true;
         var isCurrencyValid = true;
         var message = "<ul style='decorations:none'>";
         var email = this.customEmail;
+        var emailPattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
 
         // check required fields
         $(".required").each(function (index) {
@@ -105,9 +150,15 @@ class Validator {
         }
         else $(".validator-message").html('');
 
-        //console.log($(".validator-message").html());
         var retVal = isValid && isEmailValid;
         return retVal;
+    }
+
+    // clear all items with validator-message class
+    clearMessages() {
+        $(".validator-message").each(function (index) {
+            $(this).html("");
+        });
     }
 
     
